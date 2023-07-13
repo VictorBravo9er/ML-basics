788b65de3774d07fcef6e08425bc6484772cc089
Dipankar Gogoi <42418306+nanoCHI@users.noreply.github.com>
2023-07-13 22:32:22 +0530

    a

--- oirganise.py
+++ oirganise.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 def data_organise():
-    data = pd.read_csv("./real_estate.csv")
+    data = pd.read_csv("res/real_estate.csv")
     
     # 1st col to 13 (0-12) in features (rest)
     # 14 col in prediction (MEDV)
 
-    data_feature = data[data['CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD','TAX','PTRATIO','B','LSTAT']]
+    
+
+    
