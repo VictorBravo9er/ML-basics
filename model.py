3905cf8fbf3533c44ec4edb81ac89c7efaadf837
VictorBravo9er <vikramjitborah@gmail.com>
2023-07-13 16:08:32 +0530

    pl

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import numpy as np
 
 def process(attr: np.ndarray, pred: np.ndarray, weights:np.ndarray = None):
-    if not weights:
-        pass
-    
+    if isinstance(weights, np.ndarray):
+        assert weights
